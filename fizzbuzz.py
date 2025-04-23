4e2c7110a5841b9619daac2f31340d84c9f7d5f9
류지헌 <jhryu@datastreams.co.kr>
2025-04-23 18:12:48 +0900

    feat: implement fizzbuzz condition in oneline

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,6 @@
-print("ok")
+# if문 1개로 fizzbuzz 구현하기
+for i in range(1, 20+1):
+    if i % 3 == 0 or i % 5 == 0:
+        print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
+    else:
+        print(f'{i}')
